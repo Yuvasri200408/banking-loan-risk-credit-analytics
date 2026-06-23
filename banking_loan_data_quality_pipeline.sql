a930f21e6b60d2e420e0d15d0ad87e67b88a7a05
Yuvasri <yuvasri24ba@gmail.com>
2026-06-23 15:41:52 +0530

    Add files via upload

---
+++ banking_loan_data_quality_pipeline.sql
@@ -0,0 +1,68 @@
+-- BANK LOAN DATA 
+USE banking_loan_risk;
+
+-- 1. DATA TYPE 
+ALTER TABLE bank_loan
+MODIFY person_age INT,
+MODIFY person_income INT,
+MODIFY loan_amnt INT,
+MODIFY cb_person_cred_hist_length INT;
+
+-- 2. NULL CHECK 
+
+SELECT
+    SUM(person_age IS NULL) AS person_age_nulls,
+    SUM(person_gender IS NULL) AS person_gender_nulls,
+    SUM(person_education IS NULL) AS person_education_nulls,
+    SUM(person_income IS NULL) AS person_income_nulls,
+    SUM(person_emp_exp IS NULL) AS person_emp_exp_nulls,
+    SUM(person_home_ownership IS NULL) AS person_home_nulls,
+    SUM(loan_amnt IS NULL) AS loan_amount_nulls,
+    SUM(loan_intent IS NULL) AS loan_intent_nulls,
+    SUM(loan_int_rate IS NULL) AS loan_interest_nulls,
+    SUM(loan_percent_income IS NULL) AS loan_percent_income_nulls,
+    SUM(cb_person_cred_hist_length IS NULL) AS credit_history_nulls,
+    SUM(credit_score IS NULL) AS credit_score_nulls,
+    SUM(previous_loan_defaults_on_file IS NULL) AS previous_default_nulls,
+    SUM(loan_status IS NULL) AS loan_status_nulls
+FROM bank_loan;
+
+-- 3. DATA QUALITY CHECK - (INVALID VALUES)
+
+SELECT
+    SUM(person_age < 18 OR person_age > 100) AS invalid_age,
+    SUM(person_income < 0) AS invalid_income,
+    SUM(loan_amnt <= 0) AS invalid_loan_amount,
+    SUM(credit_score < 300 OR credit_score > 850) AS invalid_credit_score,
+    SUM(loan_percent_income < 0) AS invalid_loan_percent_income
+FROM bank_loan;
+
+-- 4. REMOVE INVALID AGE RECORDS
+
+SET SQL_SAFE_UPDATES = 0;
+
+DELETE FROM bank_loan
+WHERE person_age < 18 OR person_age > 100;
+
+SET SQL_SAFE_UPDATES = 1;
+
+-- 5. VERIFY CLEANING
+
+SELECT COUNT(*) AS remaining_invalid_age
+FROM bank_loan
+WHERE person_age < 18 OR person_age > 100;
+
+-- 6. CATEGORICAL VALUE CHECK
+
+SELECT DISTINCT person_gender FROM bank_loan;
+SELECT DISTINCT person_education FROM bank_loan;
+SELECT DISTINCT person_home_ownership FROM bank_loan;
+SELECT DISTINCT loan_intent FROM bank_loan;
+SELECT DISTINCT previous_loan_defaults_on_file FROM bank_loan;
+SELECT DISTINCT loan_status FROM bank_loan;
+
+-- 7. CREATE CLEAN ANALYSIS LAYER
+
+CREATE VIEW loan_cleaned AS
+SELECT * FROM bank_loan;
+
---
+++ banking_loan_risk_analysis.sql
@@ -0,0 +1,136 @@
+USE banking_loan_risk;
+-- 1. OVERALL DEFAULT RATE
+
+SELECT 
+    loan_status,
+    COUNT(*) AS total,
+    ROUND(COUNT(*) * 100.0 / (SELECT COUNT(*) FROM loan_cleaned), 2) AS percentage
+FROM loan_cleaned
+GROUP BY loan_status;
+
+
+
+-- 2. INCOME RISK ANALYSIS
+SELECT 
+    CASE 
+        WHEN person_income < 30000 THEN 'Low Income'
+        WHEN person_income BETWEEN 30000 AND 70000 THEN 'Mid Income'
+        ELSE 'High Income'
+    END AS income_band,
+
+    COUNT(*) AS total_loans,
+    SUM(CASE WHEN loan_status = 1 THEN 1 ELSE 0 END) AS defaults,
+    ROUND(SUM(CASE WHEN loan_status = 1 THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2) AS default_rate
+FROM loan_cleaned
+GROUP BY income_band
+ORDER BY default_rate DESC;
+
+-- 3. CREDIT SCORE ANALYSIS
+
+SELECT 
+    CASE 
+        WHEN credit_score < 580 THEN 'Poor'
+        WHEN credit_score BETWEEN 580 AND 669 THEN 'Fair'
+        WHEN credit_score BETWEEN 670 AND 739 THEN 'Good'
+        WHEN credit_score BETWEEN 740 AND 799 THEN 'Very Good'
+        ELSE 'Excellent'
+    END AS credit_band,
+
+    COUNT(*) AS total_loans,
+    SUM(CASE WHEN loan_status = 1 THEN 1 ELSE 0 END) AS defaults,
+    ROUND(SUM(CASE WHEN loan_status = 1 THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2) AS default_rate
+FROM loan_cleaned
+GROUP BY credit_band
+ORDER BY default_rate DESC;
+
+-- 4. DTI (DEBT-TO-INCOME) ANALYSIS
+
+SELECT 
+    CASE 
+        WHEN loan_percent_income < 0.1 THEN 'Low DTI'
+        WHEN loan_percent_income BETWEEN 0.1 AND 0.3 THEN 'Medium DTI'
+        ELSE 'High DTI'
+    END AS dti_band,
+
+    COUNT(*) AS total_loans,
+    SUM(CASE WHEN loan_status = 1 THEN 1 ELSE 0 END) AS defaults,
+    ROUND(SUM(CASE WHEN loan_status = 1 THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2) AS default_rate
+FROM loan_cleaned
+GROUP BY dti_band
+ORDER BY default_rate DESC;
+
+-- 5. RISK SCORE- FEATURE ENGINEERING
+SELECT 
+    *,
+
+    (
+        CASE 
+            WHEN person_income < 30000 THEN 30
+            WHEN person_income BETWEEN 30000 AND 70000 THEN 15
+            ELSE 5
+        END +
+
+        CASE 
+            WHEN loan_percent_income > 0.3 THEN 40
+            WHEN loan_percent_income BETWEEN 0.1 AND 0.3 THEN 20
+            ELSE 5
+        END +
+
+        CASE 
+            WHEN credit_score < 580 THEN 30
+            WHEN credit_score BETWEEN 580 AND 669 THEN 15
+            ELSE 5
+        END
+    ) AS risk_score
+
+FROM loan_cleaned;
+
+-- 6. APPROVAL DECISION ENGINE
+SELECT 
+    *,
+
+    CASE 
+        WHEN (
+            CASE 
+                WHEN person_income < 30000 THEN 30
+                WHEN person_income BETWEEN 30000 AND 70000 THEN 15
+                ELSE 5
+            END +
+
+            CASE 
+                WHEN loan_percent_income > 0.3 THEN 40
+                WHEN loan_percent_income BETWEEN 0.1 AND 0.3 THEN 20
+                ELSE 5
+            END +
+
+            CASE 
+                WHEN credit_score < 580 THEN 30
+                WHEN credit_score BETWEEN 580 AND 669 THEN 15
+                ELSE 5
+            END
+        ) >= 70 THEN 'REJECT'
+
+        WHEN (
+            CASE 
+                WHEN person_income < 30000 THEN 30
+                WHEN person_income BETWEEN 30000 AND 70000 THEN 15
+                ELSE 5
+            END +
+
+            CASE 
+                WHEN loan_percent_income > 0.3 THEN 40
+                WHEN loan_percent_income BETWEEN 0.1 AND 0.3 THEN 20
+                ELSE 5
+            END +
+
+            CASE 
+                WHEN credit_score < 580 THEN 30
+                WHEN credit_score BETWEEN 580 AND 669 THEN 15
+                ELSE 5
+            END
+        ) BETWEEN 40 AND 69 THEN 'REVIEW'
+
+        ELSE 'APPROVE'
+    END AS approval_decision
+
+FROM loan_cleaned;
